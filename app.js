e8289cc075f46e688a49343bede1a46efc7c3c3c
Zha <chacha0238@gmail.com>
2022-05-13 23:25:00 +0800

    setting template engine: handlebars

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 
 const port = 3000
 
+// setting template engine
+app.engine('handlebars', exphbs({ default: "main" }))
+app.set('view engine', 'handlebars')
 // setting the route and corresponding response
 app.get('/', (req, res) => {
-  res.send(`hahaha`)
   res.render('index')
 })
+// dynamic routes with params ':restaurant_id'
 app.get('restaurants/:restaurant_id', (req, res) => {
   res.render('show')
 })
